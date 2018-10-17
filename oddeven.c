edefd5c603d82b9804db46fb2c6dcce5e0c03ad9
anmolkumar1 <36927117+anmolkumar1@users.noreply.github.com>
2018-10-17 09:50:28 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n==0)
+{
+ printf("zero");
+ }
+ else if(n%2!=0)
+ {
+ printf("odd");
+ }
+ else 
+ {
+ printf("even");
+ }
+ return 0;
+ }
